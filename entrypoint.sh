4309c7c0a29830405da72290e6cc6e7c014b8eea
sh0327akg <shota.gk0327m.tf@gmail.com>
2023-01-30 14:41:51 +0900

    entrypoint.shの修正

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 # 自分のアプリに合わせて必要なコマンドを修正してください
 bin/rails db:migrate
 bin/rails db:seed
-yarn build --watch
-yarn build:css --watch
+# yarn build --watch
+# yarn build:css --watch
 
 rm -f tmp/pids/server.pid && bin/rails s
