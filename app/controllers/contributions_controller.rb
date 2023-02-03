625d8cc4dbab0088343adf1ff5f7aaff3c48eda3
sh0327akg <shota.gk0327m.tf@gmail.com>
2023-02-03 15:29:24 +0900

    クエリを動的に変更できるように変数を設定

--- app/controllers/contributions_controller.rb
+++ app/controllers/contributions_controller.rb
@@ -2,16 +2,16 @@ class ContributionsController < ApplicationController
   before_action :result, only: [:index]
 
   Query = MyPortfolio::Client.parse <<-'GRAPHQL'
-      query {
-        user(login: "sh0327akg") {
-          contributionsCollection {
-            contributionCalendar {
-              totalContributions
+            query($userName: String!) {
+              user(login: $userName) {
+                contributionsCollection {
+                  contributionCalendar {
+                    totalContributions
+                  }
+                }
+              }
             }
-          }
-        }
-      }
-    GRAPHQL
+          GRAPHQL
 
   def index; end
 
@@ -19,8 +19,7 @@ class ContributionsController < ApplicationController
 
   private
 
-  def result
-    result = MyPortfolio::Client.query(Query)
-    @contribution_number = result.data.user.contributions_collection.contribution_calendar.total_contributions
+  def result(variables = {})
+    reaponse = MyPortfolio::Client.query(Query, variables: variables)
   end
 end
