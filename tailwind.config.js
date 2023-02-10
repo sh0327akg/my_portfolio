22d9ee649cc1a9dde67aa34341940f52ecb2cc0f
sh0327akg <shota.gk0327m.tf@gmail.com>
2023-02-10 16:45:23 +0900

    tailwindの設定を追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,20 @@ module.exports = {
   plugins: [
     require("daisyui"),
   ],
+  theme: {
+    fontSize: {
+      sm: '0.8rem',
+      base: '1rem',
+      xl: '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
+    },
+    container: {
+      center: true,
+    },
+  },
   daisyui:{
     themes:["emerald"],
   },
