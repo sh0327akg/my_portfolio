c1ddb09a345864ad2a5a06876f4728763ffa1168
sh0327akg <shota.gk0327m.tf@gmail.com>
2023-02-12 22:49:43 +0900

    deviseを使った認証のためのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniaurh_callbacks' }
+
+  devise_scope :user do
+    get 'sign_in',  :to => 'devise/sessions#new',     :as => :new_user_session
+    get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
+
   root to: 'static_pages#top'
 
   resources :contributions
