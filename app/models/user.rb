dc925570a8e943295500a731cc46b08720a6fd73
sh0327akg <shota.gk0327m.tf@gmail.com>
2023-02-13 00:04:17 +0900

    userモデルでコールバックされた際に、アプリに登録されていないユーザーの場合は新規で登録できるように値をセットするメソッドを定義した

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,5 +25,15 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[github]
+
+  validates :uid, presence: true, uniqueness: { scope: :provider }
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email 
+      user.password = Devise.friendly_token[0, 20]
+      user.nickname = auth.info.nickname
+    end
+  end
 end
