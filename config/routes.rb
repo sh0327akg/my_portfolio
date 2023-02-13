757a53f35a583f026a70941c46902884c741ca87
sh0327akg <shota.gk0327m.tf@gmail.com>
2023-02-13 12:46:22 +0900

    ルーティングを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniaurh_callbacks' }
-
-  devise_scope :user do
-    get 'sign_in',  :to => 'devise/sessions#new',     :as => :new_user_session
-    get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
-  end
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   root to: 'static_pages#top'
 
