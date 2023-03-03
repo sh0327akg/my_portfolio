c09d3bf1b2b462950be7c0946708948ebc3b29f7
sh0327akg <shota.gk0327m.tf@gmail.com>
2023-03-03 23:29:40 +0900

    add:地図にピンを表示し、現在の草の数を表示させる

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: %i[show]
+
   def show
     @user = User.find(params[:id])
+    gon.mountains = @user.mountains.distinct
   end
 end
