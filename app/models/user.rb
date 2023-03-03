5c76ef78e5b176162e1ca410435c6736cd4a501f
sh0327akg <shota.gk0327m.tf@gmail.com>
2023-03-03 23:19:29 +0900

    modify:アソシエーションを修正し、多対対の記述を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,7 +28,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[github]
   
   has_many :contributions, dependent: :destroy
-  has_many :mountains, through: :contribution
+  has_many :mountains, through: :contributions
 
   validates :uid, presence: true, uniqueness: { scope: :provider }
 
