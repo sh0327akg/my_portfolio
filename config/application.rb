0ff5e06abb575d987549b05c377deeb7e01e841e
sh0327akg <shota.gk0327m.tf@gmail.com>
2023-03-05 23:48:13 +0900

    add: rails-i18nの設定を追加

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,11 @@ module MyPortfolio
     # in config/environments, which are processed later.
     #
     config.time_zone = "Tokyo"
+    config.active_record.default_timezone = :local
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
 
     # Don't generate system test files.
     config.generators do |g|
