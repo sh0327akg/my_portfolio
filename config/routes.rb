a8ce24b8c458f437150ce55147039fc006af4aec
sh0327akg <shota.gk0327m.tf@gmail.com>
2023-03-07 10:04:39 +0900

    modify:ルーティングを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root to: 'static_pages#top'
 
+  get "/auth/github/callback", to: "sessions#create"
+  get "/auth/failure", to: "sessions#failure"
+  delete "/sign_out", to: "sessions#destroy"
+
   resources :contributions
   resources :users, only: %i[show]
 end
