f1c7d9362cd51d86a0202d4c7115fdfa5b851765
sh0327akg <shota.gk0327m.tf@gmail.com>
2023-03-07 10:06:32 +0900

    add:テストで使うファイルを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  nickname   :string           default(""), not null
+#  provider   :string           default(""), not null
+#  uid        :string           default(""), not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_nickname  (nickname) UNIQUE
+#
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
