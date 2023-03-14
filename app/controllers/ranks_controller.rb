efd5fe13b3cf2790a788cab8d394a7662f2cbea7
sh0327akg <shota.gk0327m.tf@gmail.com>
2023-03-14 13:32:58 +0900

    add:ranksコントローラーを作成し、最新のContributionのnumberで並び替えを行う

---
+++ app/controllers/ranks_controller.rb
@@ -0,0 +1,10 @@
+class RanksController < ApplicationController
+  def rank
+    user_contributions = {}
+    User.find_each do |user|
+      latest_contribution = user.contributions.order(created_at: :desc).first
+      user_contributions[user] = latest_contribution if latest_contribution
+    end
+    @user_contribution_ranks = user_contributions.sort_by { |key, value| value.contribution_number }.reverse
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,6 @@
 class UsersController < ApplicationController
   before_action :require_login
 
-  def index
-    @users = User.includes(:contributions).order(contribution_number: :desc)
-  end
-
   def show
     @user = User.find(params[:id])
     gon.mountains = @user.mountains.distinct
