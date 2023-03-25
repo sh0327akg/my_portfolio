d474ac9a4aaee494ea0b6404ca037a5533598bed
sh0327akg <shota.gk0327m.tf@gmail.com>
2023-03-25 12:59:13 +0900

    add:ランキングにもページネーションを適用

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
 
   def contribution_history
     @user = User.find(params[:id])
-    @contributions = @user.contribution_history.page(params[:page]).per(10)
+    @contributions = @user.contribution_history.page(params[:page]).per(25)
   end
 
   private
