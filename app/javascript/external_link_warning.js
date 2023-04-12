bc35174277b8abd4332f96280178cfa747040d43
sh0327akg <shota.gk0327m.tf@gmail.com>
2023-04-12 21:48:39 +0900

    add:アラートメッセージを変更する

--- app/javascript/external_link_warning.js
+++ app/javascript/external_link_warning.js
@@ -4,7 +4,7 @@ function externalLinkWarning() {
   externalLinks.forEach(link => {
     link.addEventListener("click", function(event) {
       event.preventDefault();
-      const proceed = confirm("このリンクは外部サイトに遷移します。続行しますか？");
+      const proceed = confirm("このリンクはユーザーのGithubページに遷移します。続行しますか？");
       if (proceed) {
         window.open(link.href, "_blank");
       }
